564aa641861940f1a1df40ec014850cc1cadd6ea
Abbosdev <AbbosAlisherovich2003@gmail.com>
2022-12-17 15:37:16 +0500

    added EntityFrameworkCore NuGet Package

---
+++ DataAccess/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+    }
+}
