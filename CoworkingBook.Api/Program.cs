2861599ea9be66a89277ef437224bcb6d6b91cbb
Abbosdev <AbbosAlisherovich2003@gmail.com>
2022-12-17 23:43:24 +0500

    for check

--- CoworkingBook.Api/Program.cs
+++ CoworkingBook.Api/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("CoworkingBookingDb")));
 builder.Services.AddTransient<IGenericCRUDService<BranchModel>, MockService>();
 builder.Services.AddTransient<IGenericRepository<BranchModel>, BranchesRepository>();
-
+//for check
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
